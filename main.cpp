3499b6179401f5d2a3a0d643ad160c72577cd653
justin-schwinn-0 <justinsch1029@gmail.com>
2024-11-07 23:56:02 -0600

    Make mutex

--- main.cpp
+++ main.cpp
@@ -173,7 +173,7 @@ void runAlg(NodeInfo& ni)
 
     mrc.request();
 
-    std::thread releaseThrd(&Node::releaseMessagesThread, &ni.n,150);
+    std::thread releaseThrd(&Node::releaseMessagesThread, &ni.n,200);
     releaseThrd.detach();
 
     ni.n.listenToNeighbors();

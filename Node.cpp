98d62b1750e5433028d05f5408504032f0470636
justin-schwinn-0 <justinsch1029@gmail.com>
2024-11-08 01:02:41 -0600

    Make mutex

--- Node.cpp
+++ Node.cpp
@@ -247,6 +247,7 @@ void Node::recvMsg(int fd)
 
     if(in > 0)
     {
+        Utils::log("flags:",flags);
         std::string strMsg(buf);
         Utils::log("                           got:", strMsg);
         //std::cout << "             stream : " << sndrcvinfo.sinfo_stream << std::endl;

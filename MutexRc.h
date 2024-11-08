5814496e3df1886eb3c5f76606f855ab23592ed9
justin-schwinn-0 <justinsch1029@gmail.com>
2024-11-07 21:30:00 -0600

    Make mutex

--- MutexRc.h
+++ MutexRc.h
@@ -33,7 +33,7 @@ public:
     void releaseKeys();
 
     bool hasRequest()
-    {   return mRequestTime == INT_MAX; }
+    {   return mRequestTime != INT_MAX; }
 
     std::string getCtrlStr(const int ctrlMsgId);
 

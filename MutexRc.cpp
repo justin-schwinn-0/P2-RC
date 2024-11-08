5522da07cdee5a66f1a909f16615a726ef852649
justin-schwinn-0 <justinsch1029@gmail.com>
2024-11-07 20:59:57 -0600

    Make mutex

--- MutexRc.cpp
+++ MutexRc.cpp
@@ -24,7 +24,7 @@ void MutexRc::handleMsg(std::string msg)
     if(splits.size() == 3)
     {
         int uid = Utils::strToInt(splits[0]);
-        std::sttring msgId = splits[1]; 
+        std::string msgId = splits[1]; 
         int timeStamp = Utils::strToInt(splits[2]); 
 
         switch(msgId)

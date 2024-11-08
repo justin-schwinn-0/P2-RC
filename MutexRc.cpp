d9eabb845cf4428b475ebc54cbce4980ff3eae7a
justin-schwinn-0 <justinsch1029@gmail.com>
2024-11-07 20:43:13 -0600

    Make mutex

--- MutexRc.cpp
+++ MutexRc.cpp
@@ -26,6 +26,21 @@ void MutexRc::init()
 {
 }
 
+void MutexRc::request()
+{
+    for(auto it : mKeys)
+    {
+        if(it->second)
+        {
+            Utils::log("Have key",it->first);
+        }
+        else
+        {
+            Utils::log("Dont have Key",it->first);
+        }
+    }
+}
+
 std::string MutexRc::getCtrlStr(const std::string& ctrlMsgId)
 {
     return std::to_string(rNode.getUid()) + APP_DELIM + ctrlMsgId;
